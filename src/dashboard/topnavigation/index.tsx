e67b8dd0523def7ada64ee88c92a540e1576f906
Mahi Fernando <mahifernando@gmail.com>
2023-03-04 10:46:54 +0000

    chore: fix link

--- src/dashboard/topnavigation/index.tsx
+++ src/dashboard/topnavigation/index.tsx
@@ -1,5 +1,6 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { useToggle } from '../provider/context';
+import Link from 'next/link'
 
 export default function TopNavigation() {
   const { toggle }: any = useToggle();
@@ -51,7 +52,8 @@ export default function TopNavigation() {
                 />
               </svg>
             </a>
-            <a href="/user/messages" className="block pr-5">
+            <Link href="/user/messages" className="block pr-5">
+            {/* <a href="/user/messages" className="block pr-5"> */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6 text-gray-600"
@@ -61,7 +63,8 @@ export default function TopNavigation() {
                 <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
                 <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
               </svg>
-            </a>
+            {/* </a> */}
+            </Link>
             <a href="#" className="block relative">
               {session ? 
               (<img
