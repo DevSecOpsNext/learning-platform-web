255a2dea7c43c372fd4ca7a7352dc2f927585334
Mahi Fernando <mahifernando@gmail.com>
2023-04-22 07:42:44 +0100

    feat: initial subgraph for content api

--- services/content-api/index.ts
+++ services/content-api/index.ts
@@ -1,8 +1,35 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, json } from 'express';
+import { buildSubgraphSchema } from '@apollo/subgraph';
+import { ApolloServer } from "@apollo/server";
+import gql from 'graphql-tag';
+import { expressMiddleware } from '@apollo/server/express4';
 import 'dotenv/config';
 
 const app: Express = express();
 const port = process.env.PORT || 3003;
+
+app.use(json());
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const server = new ApolloServer({
+    schema: buildSubgraphSchema({ typeDefs, resolvers }),
+});
+
+await server.start();
+app.use(expressMiddleware(server));
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send("Content API!")
